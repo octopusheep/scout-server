436c19c4dbde2ab81a2cd935095c731cec523655
octopusheep <octopusheep@gmail.com>
2020-06-04 18:11:56 +0800

    feat: add code at app.js
    Signed-off-by: octopusheep <octopusheep@gmail.com>

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const router = require('koa-router')();
 const app = new Koa();
 
 var cors = require('koa2-cors');
-
+app.use(async (ctx, next)=> {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (ctx.method == 'OPTIONS') {
+      ctx.body = 200; 
+    } else {
+      await next();
+    }
+  });
 //允许跨域
 app.use(cors());
 
